3322dc7e41eb0cc21e2707a136ceef269624bc11
Anil Bridgpal <bridgpal@gmail.com>
2023-06-27 20:36:34 -0400

    refactored code

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Image from 'next/image'
 import { Inter } from 'next/font/google'
 
 
-export default function Home({posts}) {
+export default function Home({ posts }) {
   return (
     <main className={`flex flex-col items-center justify-between p-24`}>
       <div className="fixed top-0 left-0 w-full bg-black py-4">
@@ -14,55 +14,57 @@ export default function Home({posts}) {
           </div>
         </nav>
       </div>
-      
-     
+
+
 
       <div className="grid grid-cols-3 gap-4 mt-8 w-2/3">
-      
-    
-      {posts.map((p, index) => {
-            return (
-                <div className="bg-white rounded-lg shadow-lg p-4" key={index}>
-                <h2 className="text-lg font-bold mt-4">{p.node.title}</h2>
-                <p className="text-gray-600">{p.node.publishedDate}</p>
-              </div>
-            );
-      })}
+
+
+        {posts.map((p, index) => {
+          return (
+            <div className="bg-white rounded-lg shadow-lg p-4" key={index}>
+              <h2 className="text-lg font-bold mt-4">{p.node.title}</h2>
+              <p className="text-gray-600">{p.node.publishedDate}</p>
+            </div>
+          );
+        })}
 
       </div>
     </main>
   )
 }
 export async function getStaticProps() {
-  const apiUrl = 'https://contentful-6ljs2t-prod.valhalla-api.io/';
+const APIURL = 'https://contentful-6ljs2t-prod.valhalla-api.io/';
 
-  const response = await fetch(apiUrl, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-   
-    },
-    body: JSON.stringify({
-      query: `
-      query MyQuery {
-        allContentfulPageBlogPost(filter: {node_locale: {eq: "en-US"}}) {
-          edges {
-            node {
-              title
-              featuredImage {
-                url
-              }
-              node_locale
-              publishedDate
+  const QUERY = `
+    query MyQuery {
+      allContentfulPageBlogPost(filter: {node_locale: {eq: "en-US"}}) {
+        edges {
+          node {
+            title
+            featuredImage {
+              url
             }
+            node_locale
+            publishedDate
           }
         }
       }
-      `,
+    }
+  `
+
+  const RESPONSE = await fetch(APIURL, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+
+    },
+    body: JSON.stringify({
+      query: QUERY,
     }),
   });
 
-  const { data } = await response.json();
+  const { data } = await RESPONSE.json();
   console.log("API", data);
 
   return {
