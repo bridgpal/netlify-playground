10113a8efb5cb1238af9265875f13e04e047044e
Anil Bridgpal <bridgpal@gmail.com>
2023-07-06 15:33:27 -0400

    added imessage style card

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta property="og:title" content="iPhone" />
+        <meta property="og:image" content="https://www.apple.com/v/iphone/home/t/images/home/og.png?201610171354" />
+      </Head>
       <body>
         <Main />
         <NextScript />
